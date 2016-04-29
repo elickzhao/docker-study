c3431a3f3e7591ba558511a066bbc814017ac8b9
elickzhao <xwiwi@foxmail.com>
2016-04-30 00:01:15 +0800

    第二天修改

--- new/nginx/nginx-start.sh
+++ new/nginx/nginx-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
+cp /opt/etc/nginx.conf /etc/nginx/nginx.conf	# 为了避免第二次启动找不到替换变量 %fpm-ip%
 sed -i "s/%fpm-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
 echo "nginx start ......"
 exec /usr/sbin/nginx
