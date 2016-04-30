83a703af222de0f5b53feefb0974e6dd3bb65193
elickzhao <xwiwi@foxmail.com>
2016-04-30 23:36:34 +0800

    增加了php新的库 原版中没有mysqli 所以以后增加库都这么用  而且把Nginx也修改了 去掉了启动shell 这样就得写死php-fpm的IP  明天用个更好的方法 就是拼接conf的方法 把php-fpm放到一个文件里 这样可以手动改了 而且多个虚拟机 也可以自己调对应那个php-fmp

--- www/public/index.php
+++ www/public/index.php
@@ -27,7 +27,7 @@ $username = "root";
 $password = "123456";
 
 // 创建连接
-$conn = new mysql($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password);
 
 // 检测连接
 if ($conn->connect_error) {
